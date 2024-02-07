dde557cc31df6a2ac541d062c0764108f8399fd4
YASMINE <y_habi@estin.dz>
2024-02-07 14:45:06 +0100

    add bcrypt

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+const bcrypt = require('bcrypt');
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
